c2dd15f1af94b2767ff496ed2b478042a714b4cf
brenomarq <brenomarques516@gmail.com>
2024-06-20 16:28:04 -0300

    Implement category validation

--- src/actions/create-category.ts
+++ src/actions/create-category.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 const createCategorySchema = z.object({
   name: z.string().min(3),
-  color: z.string(),
+  color: z.enum(["red", "orange", "blue", "green", "pink"]),
 });
 
 interface createCategoryFormState {
@@ -19,8 +19,20 @@ export async function createCategory(
   formState: any,
   formData: FormData
 ): Promise<createCategoryFormState> {
-  console.log("I got called!");
+  console.log("I was called!");
 
+  const result = createCategorySchema.safeParse({
+    name: formData.get("name"),
+    color: formData.get("color"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  console.log("No problem!");
   return {
     errors: {},
   };
